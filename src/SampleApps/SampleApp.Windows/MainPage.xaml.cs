788e42497fa0998028f5457aaa72bb11f5ef8ef1
mort5161 <mnielsen@esri.com>
2015-06-15 21:31:07 -0700

    Updated sample with a brightness slider

--- src/SampleApps/SampleApp.Windows/MainPage.xaml.cs
+++ src/SampleApps/SampleApp.Windows/MainPage.xaml.cs
@@ -4,6 +4,7 @@ using System.Collections.ObjectModel;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
@@ -73,6 +74,8 @@ namespace SampleApp
 			{
 				Name.Text = await client.GetDeviceLabelAsync(bulb);
 				PowerState.IsOn = await client.GetLightPowerAsync(bulb);
+				var state = await client.GetLightStateAsync(bulb);
+				brightnessSlider.Value = state.Brightness;
 			}
 		}
 
@@ -82,6 +85,45 @@ namespace SampleApp
 			if (bulb != null)
 			{
 				await client.SetDevicePowerStateAsync(bulb, PowerState.IsOn);
+			}
+		}
+
+		private void brightnessSlider_ValueChanged(object sender, RangeBaseValueChangedEventArgs e)
+		{
+			var bulb = bulbList.SelectedItem as LifxNet.LightBulb;
+			if (bulb != null)
+				SetBrightness(bulb, e.NewValue);
+		}
+
+		private Action pendingUpdateColorAction;
+		private Task pendingUpdateColor;
+
+		private async void SetBrightness(LifxNet.LightBulb bulb, double brightness)
+		{
+			if (client == null) return;
+			//Is a task already running? This avoids updating too often.
+			//Come back and execute last call when currently running operation is complete
+			if (pendingUpdateColor != null) 
+			{
+				pendingUpdateColorAction = () => SetBrightness(bulb, brightness);
+				return;
+			}
+
+
+			var setColorTask = client.SetColorAsync(bulb, 0, 0, (UInt16)brightness, 2700, TimeSpan.Zero);
+			var throttleTask = Task.Delay(50); //Ensure task takes minimum 50 ms (no more than 20 messages per second)
+			pendingUpdateColor = Task.WhenAll(new Task[] { setColorTask, throttleTask });
+ 			try
+			{
+				await pendingUpdateColor;
+			}
+			catch { } //ignore errors (usually timeout)
+			pendingUpdateColor = null;
+			if (pendingUpdateColorAction != null) //if a pending action is waiting, run it now;
+			{
+				var a = pendingUpdateColorAction;
+				pendingUpdateColorAction = null;
+				a();
 			}
 		}
 	}
