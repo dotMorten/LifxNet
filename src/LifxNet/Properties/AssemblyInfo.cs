631c5f352d2aa46d1257ffae1d33f4be9987dc41
Morten Nielsen <mn@iter.dk>
2015-06-16 23:45:07 -0700

    Added nuget build scripts

--- src/LifxNet/Properties/AssemblyInfo.cs
+++ src/LifxNet/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("LifxNet")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyCopyright("Copyright © Morten Nielsen 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
