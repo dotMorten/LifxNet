e5c327e67c1afdfc5808bd0ab5bf056a46a7724f
Morten Nielsen <mn@iter.dk>
2016-12-27 21:40:24 -0800

    Update version number to 2.1

--- src/LifxNet/Properties/AssemblyInfo.cs
+++ src/LifxNet/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("LifxNet")]
-[assembly: AssemblyCopyright("Copyright © Morten Nielsen 2015")]
+[assembly: AssemblyCopyright("Copyright © Morten Nielsen 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -24,6 +24,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.0.0.0")]
-[assembly: AssemblyFileVersion("2.0.0.0")]
+[assembly: AssemblyVersion("2.1.0.0")]
+[assembly: AssemblyFileVersion("2.1.0.0")]
 [assembly: ComVisible(false)]
